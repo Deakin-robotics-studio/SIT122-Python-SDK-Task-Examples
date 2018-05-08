831e6b0045717d3303877a1524f4d9729fc7e35e
Richard Denton <isdampe@gmail.com>
2018-05-08 11:53:07 +1000

    Rename task-18.py to ReactToWords

---
+++ Task-18/ReactToWords.py
@@ -0,0 +1,58 @@
+from naoqi import ALProxy, ALBroker, ALModule
+from time import sleep
+
+robotIP = "localhost"
+robotPort = 9559
+CONFIDENCE_THRESHOLD = 0.5
+
+class SpeechRecognition(ALModule):
+
+	def __init__(self, name):
+		# Register our module with naoqi
+		ALModule.__init__(self, name)
+
+		self.memory = ALProxy("ALMemory")
+
+		self.asr = ALProxy("ALSpeechRecognition")
+		self.asr.setLanguage("English")
+		self.asr.setVocabulary(["yes", "no"], False)
+		self.asr.subscribe("SpeechRecognition")
+		self.asr.pause(False)
+
+		self.tts = ALProxy("ALTextToSpeech")
+
+		# Subscribe self.onWordRecognized to WordRecognized.
+		self.memory.subscribeToEvent("WordRecognized", "SpeechRecognition", "onWordRecognized")
+
+		self.speakLock = False
+
+	def onWordRecognized(self, ref, spot):
+		if self.speakLock == True:
+			return
+
+		self.speakLock = True
+		command = spot[0]
+		confidence = spot[1]
+		print "I heard %s with a confidence level of %s" % (command, confidence)
+		self.tts.say(command)
+		self.speakLock = False
+
+	def onEnd(self):
+		self.asr.pause(True)
+		self.quit = True
+
+	def main(ip, port):
+		global SpeechRecognition
+
+		# Setup the data broker.
+		myBroker = ALBroker("myBroker", "0.0.0.0", 0, ip, port)
+		SpeechRecognition = SpeechRecognition("SpeechRecognition")
+		try:
+			while True:
+				sleep(0.1)
+		except KeyboardInterrupt:
+			SpeechRecognition.onEnd()
+			myBroker.shutdown()
+
+if __name__ == "main":
+	main(robotIP, robotPort)
